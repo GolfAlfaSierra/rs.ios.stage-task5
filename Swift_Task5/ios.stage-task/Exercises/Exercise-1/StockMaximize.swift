ceeec05fed09ac3ff43a5a72f48ceb89b6044a1c
Artyom Sog <tomas.tomson@Mail.ru>
2021-06-12 16:20:15 +0300

    feat: exercise one tast pass

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,63 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        let len = prices.count
+        
+        if len < 1 {
+            return 0
+        }
+        
+        var total = 0
+        var stocksToBuy = [Int]()
+        
+        var counter = 0
+        
+        
+        while true {
+           
+            
+            let current = prices[counter]
+            var next = -1
+            
+            stocksToBuy.append(current)
+            
+            if counter+1 <= prices.count-1 {
+                next = prices[counter+1]
+            }
+            
+            if current < stocksToBuy.last! || counter == prices.endIndex-1 ||  next < current {
+                total += calcSub(arr: stocksToBuy)
+                stocksToBuy.removeAll()
+            }
+            
+            
+            if counter == prices.endIndex-1 {break}
+            counter+=1
+            
+            
+        }
+        
+        
+        if total < 0 {
+            return 0
+        }
+        
+        
+        
+        return total
+        
+    }
+    
+    private func calcSub(arr: [Int]) -> Int {
+        var sum = 0
+        let maxVal = arr[arr.count-1]
+        for price in arr {
+            if maxVal == price {break}
+            sum += maxVal - price
+        }
+        
+        return sum
     }
 }
