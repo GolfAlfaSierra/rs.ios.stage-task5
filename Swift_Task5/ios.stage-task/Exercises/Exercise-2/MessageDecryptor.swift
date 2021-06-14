265260fc6047c40ce461c135602a467a6ba459c9
Artyom Sog <tomas.tomson@Mail.ru>
2021-06-14 12:02:40 +0300

    feat: exercise two tast pass

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -2,7 +2,64 @@ import UIKit
 
 class MessageDecryptor: NSObject {
     
+    
+    
+    
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var multiplierNumAccummulator = ""
+        var result = ""
+        
+        var previousStringStack = [""]
+        var prefixNumStack = [""]
+        
+        
+        
+        
+        for ch in message {
+            
+            if ch.isNumber { // добавляем во временную строку с числами
+                multiplierNumAccummulator.append(ch)
+                
+            } else {
+                
+                switch ch {
+                
+                case "[": // начинается блок сообщения, надо передать множитель и последнее состояние строки с результатом
+                    
+                    if multiplierNumAccummulator == "" {
+                        prefixNumStack.append("1")
+                    } else {
+                        prefixNumStack.append(multiplierNumAccummulator)
+                    }
+                    
+                    previousStringStack.append(result) // стешим текущий result
+                    multiplierNumAccummulator = ""
+                    result = ""
+                    
+                case "]": // блок заканчивается, надо получить последний множитель, повторить строку Н раз, соеденить с предыдущей строкой в стаке, и записать в result
+                    var repeatedStr = ""
+                    let last = prefixNumStack.popLast()
+                    
+                    
+                    if let counter = Int("\(last ?? "1")") {
+                        
+                        for _ in 0..<counter {
+                            repeatedStr.append(result)
+                        }
+                    }
+                    
+                    result = "\(previousStringStack.popLast()!)\(repeatedStr)"
+                    
+                    
+                    
+                default: // стандартный сценарий, символ строка
+                    result.append(ch)
+                    
+                }
+            }
+            
+        }
+        
+        return result
     }
 }
